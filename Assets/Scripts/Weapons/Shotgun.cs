b157936bb36389b75a4f157d77a6dc902d005cff
scazzman99 <scazzman99@gmail.com>
2018-09-13 21:22:53 +1000

    Changelog 7
    Added a firerate Coroutine to shotgun. Dont know if I can just have this be held in Weapon (I probably can)

--- Assets/Scripts/Weapons/Shotgun.cs
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -6,28 +6,44 @@ public class Shotgun : Weapon {
 
     #region Shotgun Variables
     public int pellets = 6;
+    public bool canFire = true;
     #endregion
 
 
     public override void Attack()
     {
-        //store the forward direction of the player
-        Vector3 dir = transform.forward;
-        //Calculate offset using range
-        Vector3 spread = Vector3.zero;
-        //Offset on local Y
-        spread += transform.up * Random.Range(-acc * 0.2f, acc * 0.2f);
-        //Offset on local X
-        spread += transform.right * Random.Range(-acc * 0.2f, acc * 0.2f);
+        if (canFire)
+        {
+            for (int i = 0; i < pellets; i++)
+            {
+                //store the forward direction of the player
+                Vector3 dir = transform.forward;
+                //Calculate offset using range
+                Vector3 spread = Vector3.zero;
+                //Offset on local Y
+                spread += transform.up * Random.Range(-acc * 0.2f, acc * 0.2f);
+                //Offset on local X
+                spread += transform.right * Random.Range(-acc * 0.2f, acc * 0.2f);
 
 
-        //instantiate new bullet from prefab bullet
-        GameObject clone = Instantiate(projectile, spawnP.position, spawnP.rotation);
-        //get the component from new bullet
-        Bullet newBullet = clone.GetComponent<Bullet>();
-        //tell the bullet to fire in direction + spread variation
-        newBullet.Fire(dir + spread);
+                //instantiate new bullet from prefab bullet
+                GameObject clone = Instantiate(projectile, spawnP.position, spawnP.rotation);
+                //get the component from new bullet
+                Bullet newBullet = clone.GetComponent<Bullet>();
+                //tell the bullet to fire in direction + spread variation
+                newBullet.Fire(dir + spread);
+            }
+            canFire = false;
+            StartCoroutine(FireReset(1f));
+        }
 
+    }
+
+    public IEnumerator FireReset(float fireRate)
+    {
+        float timeWait = 1f / fireRate;
+        yield return new WaitForSecondsRealtime(timeWait);
+        canFire = true;
     }
 }
 
